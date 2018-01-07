b275557c1c5f64f3f9be7543285678d222f9a667
royhan97 <roy.addz@gmail.com>
2018-01-07 23:38:41 +0700

    activity_home

--- app/src/main/java/com/example/adhit/bikubiku/BikuBiku.java
+++ app/src/main/java/com/example/adhit/bikubiku/BikuBiku.java
@@ -2,6 +2,11 @@ package com.example.adhit.bikubiku;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
+
+import com.qiscus.sdk.Qiscus;
+
+import org.greenrobot.eventbus.EventBus;
 
 /**
  * Created by adhit on 03/01/2018.
@@ -17,9 +22,45 @@ public class BikuBiku extends Application{
     @Override
     public void onCreate() {
         super.onCreate();
+        initQiscus();
+        EventBus.builder().build();
         sContext = this;
-
     }
 
+    public void initQiscus(){
+        Qiscus.init(this, "bikubiku-lntq5ndpvich");
+
+        Qiscus.getChatConfig()
+                .setSwipeRefreshColorScheme(R.color.colorPrimary, R.color.colorAccent)
+                .setLeftBubbleColor(R.color.leftBubble)
+                .setLeftBubbleTextColor(R.color.qiscus_primary_text)
+                .setLeftBubbleTimeColor(R.color.qiscus_secondary_text)
+                .setLeftLinkTextColor(R.color.qiscus_primary_text)
+                .setLeftProgressFinishedColor(R.color.colorPrimary)
+                .setRightBubbleColor(R.color.colorPrimaryLight)
+                .setRightProgressFinishedColor(R.color.colorPrimaryLight)
+                .setSelectedBubbleBackgroundColor(R.color.colorPrimary)
+                .setReadIconColor(R.color.colorPrimary)
+                .setAppBarColor(R.color.colorPrimary)
+                .setStatusBarColor(R.color.colorPrimaryDark)
+                .setAccentColor(R.color.colorAccent)
+                .setAccountLinkingTextColor(R.color.colorPrimary)
+                .setAccountLinkingBackground(R.color.accountLinkingBackground)
+                .setButtonBubbleTextColor(R.color.colorPrimary)
+                .setButtonBubbleBackBackground(R.color.accountLinkingBackground)
+                .setReplyBarColor(R.color.colorPrimary)
+                .setReplySenderColor(R.color.colorPrimary)
+                .setSendButtonIcon(R.drawable.ic_default_send)
+                .setStopRecordIcon(R.drawable.ic_send_record)
+                .setEnableAddFile(true)
+                .setCancelRecordIcon(R.drawable.ic_cancel_record)
+                .setEnablePushNotification(false)
+                .setInlineReplyColor(R.color.colorPrimaryLight);
+
+        Qiscus.getChatConfig().setEnableFcmPushNotification(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Qiscus.getChatConfig().setEnableReplyNotification(true);
+        }
+    }
 
 }
